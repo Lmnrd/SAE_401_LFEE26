de1b0ebce1745ab7613ebe18ef11e53651c1163e
Lmnrd <liamandouard@gmail.com>
2026-03-13 15:11:28 +0100

    suppression de l'ancienne version
    on supprime pour avoir l'api clean

--- sae401/vendor/symfony/ux-react/assets/dist/components.d.ts
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent, ComponentClass } from 'react';
-
-type Component = string | FunctionComponent<object> | ComponentClass<object, any>;
-interface ComponentCollection {
-    [key: string]: Component;
-}
-declare const components: ComponentCollection;
-
-export { type ComponentCollection, components };
--- sae401/vendor/symfony/ux-react/assets/dist/components.js
+++
@@ -1,4 +0,0 @@
-const components = {};
-export {
-  components
-};
