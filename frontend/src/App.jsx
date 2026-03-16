b6d25b7ccb6ebc931074ee9e231a052790eb257c
Florian <floriantrelle@gmail.com>
2026-03-16 18:20:40 +0100

    ajout map france

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-import ParcSocialPage from "./pages/ParcSocialPage";
-
-function App() {
-  return (
-    <div>
-      <h1>Statistiques Logement</h1>
-      <ParcSocialPage />
-=======
 import { useEffect, useState } from "react";
+import ParcSocialPage from "./pages/ParcSocialPage";
 import MapFrance from "./components/MapFrance";
 
 function App() {
-
   const [data, setData] = useState(null);
 
   useEffect(() => {
@@ -23,12 +14,16 @@ function App() {
 
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Ma Carte de France Interactive</h1>
+      <h1>Statistiques Logement & Carte Interactive</h1>
+      
+      {/* Test de l'API de base */}
       {data && <p>{data.message}</p>}
       
-      {/* Intégration de la carte */}
+      {/* Historique: Graphiques du Parc Social */}
+      <ParcSocialPage />
+
+      {/* Intégration de la nouvelle carte */}
       <MapFrance />
->>>>>>> 66d7d7d37559777b301626b3300b8ca13dd0ec25
     </div>
   );
 }
