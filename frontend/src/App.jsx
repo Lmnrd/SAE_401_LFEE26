549feb4443ccc1d1573c57afbf753706910929df
Lmnrd <liamandouard@gmail.com>
2026-03-16 18:50:45 +0100

    resolution bug npm run dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
 import ParcSocialPage from "./pages/ParcSocialPage";
 import MapFrance from "./components/MapFrance";
-<<<<<<< HEAD
 import TauxLogementsPage from "./pages/TauxLogements";
-=======
->>>>>>> 562e7d5df9a5cc30a69eb520494d43e83a44bf96
 
 function App() {
   const [data, setData] = useState(null);
@@ -18,32 +15,19 @@ function App() {
 
   return (
     <div style={{ padding: '20px' }}>
-<<<<<<< HEAD
       <h1>Statistiques Logement & Carte Interactive</h1>
-=======
-      <h1>Carte Interactive</h1>
->>>>>>> 562e7d5df9a5cc30a69eb520494d43e83a44bf96
 
       {/* Test de l'API de base */}
       {data && <p>{data.message}</p>}
 
-<<<<<<< HEAD
       {/* Historique: Graphiques du Parc Social */}
-=======
->>>>>>> 562e7d5df9a5cc30a69eb520494d43e83a44bf96
       <ParcSocialPage />
-      <MapFrance />
-<<<<<<< HEAD
-
-
-      {/* Intégration de la carte */}
-      <MapFrance />
-
 
       {/* Taux Logements */}
       <TauxLogementsPage />
-=======
->>>>>>> 562e7d5df9a5cc30a69eb520494d43e83a44bf96
+
+      {/* Intégration de la carte */}
+      <MapFrance />
     </div>
   );
 }
